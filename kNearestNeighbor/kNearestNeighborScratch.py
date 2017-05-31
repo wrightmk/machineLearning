9d265d683f16dd572335f99e2e0459f530df061a
wrightmk <wrightmitchell@hotmail.com>
2017-05-31 00:11:37 -0700

    finished building k Nearest Neighbor from scratch using a numpy euclidean distance formula

--- kNearestNeighbor/kNearestNeighborScratch.py
+++ kNearestNeighbor/kNearestNeighborScratch.py
@@ -11,15 +11,6 @@ style.use('fivethirtyeight')
 dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5], [7,7], [8,6]]}
 new_features = [5,7]
 
-# verbose
-# for i in dataset:
-#     for ii in dataset[i]:
-#         plt.scatter(ii[0], ii[1], s=100, color=i)
-
-# [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-# plt.scatter(new_features[0], new_features[1])
-# plt.show()
-
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('K is set to a value less than total voting groups!')
@@ -31,7 +22,24 @@ def k_nearest_neighbors(data, predict, k=3):
             euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
             distances.append([euclidean_distance, group])
 
-            # episode 17
-            #5:00
+    votes = [i[1] for i in sorted(distances)[:k]]
+    print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+    #most common group [0], how many there were [0][0]
 
     return vote_result
+
+results = k_nearest_neighbors(dataset, new_features, k=3)
+print(results)
+
+# verbose
+# for i in dataset:
+#     for ii in dataset[i]:
+#         plt.scatter(ii[0], ii[1], s=100, color=i)
+
+[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1], color=results)
+plt.show()
+
+
+# resulting in new feature being closest to the r dataset as predicted
